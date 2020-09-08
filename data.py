a714dad3ae5d05c7aaa609edd654d048ab20b696
arhaanb <arhaanb@gmail.com>
2020-09-08 20:15:30 +0530

    init routes, data

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Make.',
+            'body': 'When faced with the seemingly insurmountable, we resort to our inherent ingenuity to deal with it. We make things. Things of utility. Things of majesty. Things that make you stop and think how you didn’t think of a solution so elegant to a problem so daunting.',
+            'author': 'Arhaan Bahadur',
+            'create_date': '09-08-2020'
+        },
+        {
+            'id': 2,
+            'title': 'X 2020',
+            'body': 'X is an exclusive invite-only event, witnessing participation from all over India. Invites are extended to a select number of the very best schools, based on significant achievements in the previous years. The event will consist of our online hunt, CryptX, and eleven online events — Design, Quiz, Crossword, Photography, Gaming, Programming, Filmmaking, Pitching, Robotics, Group Discussion, and a Surprise Event. Each event will have a unique theme based around Make.',
+            'author': 'Arhaan Bahadur',
+            'create_date': '10-08-2020'
+        }
+    ]
+
+    return articles
